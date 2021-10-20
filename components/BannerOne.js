aadc810431efafd8b2049adf0be958950773089a
kaaaaraaaan <tiwari.krn@gmail.com>
2021-10-20 20:53:36 +0545

    Update BannerOne.js

--- components/BannerOne.js
+++ components/BannerOne.js
@@ -25,7 +25,6 @@ export default class BannerOne extends React.Component {
                 <img src="/assets/images/shapes/banner-bg-shape-1-1.png" alt="" className="banner-one__moc"/>
                     <div className="container">
                         <div className="banner-one__video wow fadeInUp" data-wow-animation-duration="1500ms">
-                            <img src="/assets/images/resources/video-1-1.jpg" width="639" height="639" alt=""/>
                             <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?controls=0&amp;list=PLoBU8po_SBvf1BZwGO8cxjllWKKShkioP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
                                    className="video-popup banner-one__video-btn"><i className="fa fa-play"></i></iframe>
                         </div>
