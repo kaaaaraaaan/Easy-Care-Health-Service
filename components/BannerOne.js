960094bd90abac0f7f4926820d2c451f347f76ca
kaaaaraaaan <tiwari.krn@gmail.com>
2021-10-20 22:36:40 +0545

    Update BannerOne.js

--- components/BannerOne.js
+++ components/BannerOne.js
@@ -26,8 +26,7 @@ export default class BannerOne extends React.Component {
                     <div className="container">
                         <div className="banner-one__video wow fadeInUp" data-wow-animation-duration="1500ms">
                             <img src="/assets/images/resources/video-1-1.jpg" width="639" height="639" alt=""/>
-                            <a href="https://www.youtube.com/watch?v=0niXJOcjL5U&t=12s"
-                                   className="video-popup banner-one__video-btn"><i className="fa fa-play"></i></a>
+                            <div className="video-popup banner-one__video-btn"><i className="fa fa-play"><a href="https://www.youtube.com/watch?v=0niXJOcjL5U&t=12s"></i></a></div>
                         </div>
                         <div className="row">
                             <div className="col-lg-6">
